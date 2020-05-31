b7211b5b5edca237a5a4197f1b699dd758458430
Emmanuel <Emmaccen@users.noreply.github.com>
2020-05-31 18:08:31 +0100

    Update

--- src/components/ContentPage/Content.js
+++ src/components/ContentPage/Content.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import AdminHeader from '../../components/AdminHeader'
+import {AdminHeader} from '../../components/AdminHeader'
 
 
 class Content extends React.Component {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,19 @@
 import React, {Component} from 'react'
+import {openDialogu} from './HomePage'
+import $ from 'jquery'
 
 const firebase = require("firebase");
 
 class NavBar extends Component {
 
-
-    loginUser (button, loader) {
-        const load = document.getElementById('loader')
-        const btn = document.getElementById('button')
-        // notify and restrict clicks while processing
-        btn.setAttribute('disabled', 'disabled')
-        // show progress animation
-        load.classList.add('spinner-grow')
-        // authenticate user
-        firebase.auth().signInWithEmailAndPassword('oriolaemmanuel199@gmail.com', '12345678').then(function success() {
-            // remove progress animation
-            load.classList.remove('spinner-grow')
-            // Seccessfully logged in
-            btn.removeAttribute('disabled')
-          
-        }).catch(function(error) {
-            // Handle Errors here.
-            // remove progress animation
-            load.classList.remove('spinner-grow')
-            btn.removeAttribute('disabled')
-            // ...
-          });
+    constructor (props) {
+        super(props)
     }
     render() {
         
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
                     <a className="navbar-brand" href="#">
                         <img src='/Logo.svg' alt='Company Logo' ></img>
                     </a>
@@ -40,17 +22,11 @@ class NavBar extends Component {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav mr-auto">
-                            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link active" href="#">Home<span className="sr-only">(current)</span></a>
                             <a className="nav-item nav-link" href="#">Profiles</a>
                             <a className="nav-item nav-link" href="#">Admins</a>
-                        </div>
-                        <form onSubmit={e => e.preventDefault()} className="form-inline my-2 my-lg-0">
-                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </form>
-                        <button onClick={()=> this.loginUser('button', 'loader')} id='button' className="btn btn-outline-success my-2 my-sm-0 ml-5">
-                        <span id='loader' className="spinner-grow-sm" role="status" aria-hidden="true"></span>
-                        Login</button>    
+                        </div>                
+                        <button onClick={()=> openDialogu()} className="btn btn-outline-success my-2 my-sm-0 ml-0">Login</button>    
                     </div>
                 </nav>
             </div>
