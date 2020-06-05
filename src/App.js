520e994158fad75afa1191a7de73555c2a1e286c
Emmanuel <Emmaccen@users.noreply.github.com>
2020-06-06 00:15:21 +0100

    Revert "Merge branch 'master' into ContentPage"
    This reverts commit f9f54ccd746c6639ac102ee5d3aaeedeaf41a5a4, reversing
    changes made to c79e56572f4b4c96074f216906dd23558323ac0b.

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import React from 'react';
 import './App.css';
 import Application from './components/Application'
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import ViewProfile from './components/ViewProfile'
-import CreateProfile from './components/CreateProfile'
 import Content from './components/ContentPage/Content'
-import Profile from './components/Profile';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
+        <switch>
           <Route path='/' exact component={Application}></Route>
           <Route path='/Content' exact component={Content}></Route>
-          {/* <Route path='/ViewProfile' exact component={ViewProfile}></Route> */}
-          <Route path='/CreateProfile' exact component={CreateProfile}></Route>
-          <Route path='/Profile/:id' exact component={Profile}></Route>
-        </Switch>
+        </switch>
       </div>
     </Router>
   );
--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -3,33 +3,35 @@ import {Link} from 'react-router-dom'
 
 const firebase = require("firebase");
 
-function logOutUser (button, loader) {
-    const load = document.getElementById('loader')
-    const btn = document.getElementById('logout')
-    // notify and restrict clicks while processing
-    btn.setAttribute('disabled', 'disabled')
-    // show progress animation
-    load.classList.add('spinner-grow')
-    // sign out user
-      firebase.auth().signOut().then(function(){
-        // remove progress animation
-        load.classList.remove('spinner-grow')
-        // Seccessfully logged in
-        btn.removeAttribute('disabled')
-       
-    }).catch(function(error) {
-        // Handle Errors here.
-        // remove progress animation
-        load.classList.remove('spinner-grow')
-        btn.removeAttribute('disabled')
-        // ...
-      });
-}
 
 class AdminHeader extends React.Component {
 
     constructor(props) {
         super(props)
+    }
+
+
+    logOutUser (button, loader) {
+        const load = document.getElementById('loader')
+        const btn = document.getElementById('logout')
+        // notify and restrict clicks while processing
+        btn.setAttribute('disabled', 'disabled')
+        // show progress animation
+        load.classList.add('spinner-grow')
+        // sign out user
+          firebase.auth().signOut().then(function(){
+            // remove progress animation
+            load.classList.remove('spinner-grow')
+            // Seccessfully logged in
+            btn.removeAttribute('disabled')
+           
+        }).catch(function(error) {
+            // Handle Errors here.
+            // remove progress animation
+            load.classList.remove('spinner-grow')
+            btn.removeAttribute('disabled')
+            // ...
+          });
     }
 
     render () {
@@ -58,11 +60,9 @@ class AdminHeader extends React.Component {
                             <input className="form-control mr-sm-2 search" type="search" placeholder="Search By Name" aria-label="Search"></input>
                             <button onClick={this.props.search}  className="btn btn-outline-success my-2 my-sm-0" type="submit">Search Profile</button>
                         </form>
-                        <Link to='/CreateProfile'>
-                            <button className="btn btn-outline-success my-2 my-sm-0 ml-5">Create Profile</button>
-                        </Link>
+                        <button className="btn btn-outline-success my-2 my-sm-0 ml-5" data-toggle="modal" data-target="#exampleModalLong">Create Profile</button>
                         <button
-                            onClick={()=> logOutUser('button', 'loader')}
+                            onClick={()=> this.logOutUser('button', 'loader')}
                          id='logout' className="btn btn-outline-danger my-2 my-sm-0 ml-5">
                         <span id='loader' className="spinner-grow-sm" role="status" aria-hidden="true"></span>
                         Logout</button>
@@ -73,5 +73,4 @@ class AdminHeader extends React.Component {
     }
 }
 
-export { AdminHeader }
-export { logOutUser }
+export default AdminHeader
